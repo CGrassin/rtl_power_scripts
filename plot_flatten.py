73efa43a16081098192cece3ee127556a87b35d1
CGrassin <you@example.com>
2019-06-15 20:29:45 +0200

    Added time plot

--- plot_flatten.py
+++ plot_flatten.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Plot a flattened CSV from rtl_power')
 parser.add_argument('input_csv_file', help="rtl_power CSV file to process")
+parser.add_argument("-t","--time", help="plot time-grouped file",action="store_true")
 args = parser.parse_args()
 
 x = []
@@ -12,8 +13,12 @@ y = []
 
 for line in open(args.input_csv_file):
     line = line.strip().split(',')
-    x.append(float(line[0]))
     y.append(float(line[1]))
 
+    if args.time:
+        x.append(line[0])
+    else:
+        x.append(float(line[0]))
+    
 plt.plot(x,y)
 plt.show()
